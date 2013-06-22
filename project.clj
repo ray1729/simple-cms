7e68e3607d398237597acd5fff9f1bc2f445e4a5
Ray Miller <ray@1729.org.uk>
2013-06-22 12:04:41 +0100

    Update to latest version of Clojure, Compojure and Enlive

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject simple-cms "1.0.0-SNAPSHOT"
+(defproject simple-cms "1.0.1-SNAPSHOT"
   :description "Simple CMS using Compojure, Enlive and Bootstrap"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.0"]
-                 [enlive "1.0.0"]
-                 [clj-time "0.4.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [enlive "1.1.1"]
+                 [clj-time "0.5.1"]]
   :ring {:handler simple-cms.core/app
          :init simple-cms.core/init
          :destroy simple-cms.core/destroy})
